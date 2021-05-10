d0271104b747ae4915b9cc90aa59d7a07beffd4b
asalimw <57966771+asalimw@users.noreply.github.com>
2021-05-10 15:10:42 +1000

    tabular_epsilon greedy and run episode update

--- agent_tabular_ql.py
+++ agent_tabular_ql.py
@@ -14,7 +14,7 @@ NUM_RUNS = 10
 NUM_EPOCHS = 200
 NUM_EPIS_TRAIN = 25  # number of episodes for training at each epoch
 NUM_EPIS_TEST = 50  # number of episodes for testing
-ALPHA = 0.1  # learning rate for training
+ALPHA = 1  # learning rate for training
 
 ACTIONS = framework.get_actions()
 OBJECTS = framework.get_objects()
@@ -35,7 +35,37 @@ def epsilon_greedy(state_1, state_2, q_func, epsilon):
         (int, int): the indices describing the action/object to take
     """
     # TODO Your code here
+
+    # https://stackoverflow.com/questions/9482550/argmax-of-numpy-array-returning-non-flat-indices
+    # the following numpy library functions were helpful:
+    # np.random library, np.argmax, and numpy.unravel_index(indices, shape, order='C')
+    # https://stackoverflow.com/questions/53470744/find-indices-of-maximum-value-in-matrix-python
+    # https://www.geeksforgeeks.org/epsilon-greedy-algorithm-in-reinforcement-learning/
+
+    #  If we explore randomly, i.e., always select actions at random, we would most likely not get anywhere.
+    #  A better option is to exploit what we have already learned, as summarized by current Q-values.
+    #  We can always act greedily with respect to the current estimates,
+    #  i.e., take an action π(s)=argmaxc∈CQ(s,c).
+
+    # initial
     action_index, object_index = None, None
+
+    # Generate a non-uniform random result as if a were np.arange(a)
+    # https://www.sharpsightlabs.com/blog/numpy-random-choice/
+    result = np.random.choice(a=[0, 1], p=[epsilon, 1 - epsilon])
+
+    # optimal policy
+    if result == 1:
+        # fetch max argument index by np.unravel_index
+        # https://www.geeksforgeeks.org/numpy-unravel_index-function-python/
+        index = np.unravel_index(np.argmax(q_func[state_1, state_2], axis=None), q_func[state_1, state_2].shape)
+        action_index = index[0]
+        object_index = index[1]
+    # random select action-object
+    if result == 0:
+        action_index = np.random.randint(NUM_ACTIONS)
+        object_index = np.random.randint(NUM_OBJECTS)
+
     return (action_index, object_index)
 
 
@@ -62,8 +92,15 @@ def tabular_q_learning(q_func, current_state_1, current_state_2, action_index,
     """
     # TODO Your code here
 
+    # global update
+    # parameter value will be overwritten even outside the function
+
+    # q_func = np.zeros((NUM_ROOM_DESC, NUM_QUESTS, NUM_ACTIONS, NUM_OBJECTS))
+    # should give some insight in what each dimension represents.
+
+    # TODO Your update here
     # If terminal step Q(s',c') = 0
-    if terminal:
+    if terminal == True:
         maxQ = 0
     else:
         maxQ = np.max(q_func[next_state_1, next_state_2, :, :])
@@ -93,28 +130,50 @@ def run_episode(for_training):
     """
     epsilon = TRAINING_EP if for_training else TESTING_EP
 
-    epi_reward = None
+
     # initialize for each episode
     # TODO Your code here
 
     (current_room_desc, current_quest_desc, terminal) = framework.newGame()
+    # initial value
+    count = 0
+    epi_reward = 0
 
     while not terminal:
         # Choose next action and execute
         # TODO Your code here
 
+        # recall index from dictionary by "description" key
+        current_state_1 = dict_room_desc[current_room_desc]
+        current_state_2 = dict_quest_desc[current_quest_desc]
+
+        (action_index, object_index) = epsilon_greedy(current_state_1, current_state_2, q_func, epsilon)
+
+        (next_room_desc, next_quest_desc, reward, terminal) \
+            = framework.step_game(current_room_desc, current_quest_desc, action_index, object_index)
+
+        next_state_1 = dict_room_desc[next_room_desc]
+        next_state_2 = dict_quest_desc[next_quest_desc]
+
         if for_training:
             # update Q-function.
             # TODO Your code here
+            tabular_q_learning(q_func, current_state_1, current_state_2, action_index, object_index,
+                               reward, next_state_1, next_state_2, terminal)
+
             pass
 
         if not for_training:
             # update reward
             # TODO Your code here
+            epi_reward += np.power(GAMMA, count) * reward
             pass
 
         # prepare next step
         # TODO Your code here
+        count += 1
+        current_room_desc = next_room_desc
+        current_quest_desc = next_quest_desc
 
     if not for_training:
         return epi_reward
